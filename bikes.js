5678842cf78b63425f8b6f524a29acac1acc4a1c
J. Haskins <ziopads@gmail.com>
2016-11-22 15:35:37 -0700

    remove conditional for test

--- bikes.js
+++ bikes.js
@@ -22,27 +22,27 @@ $(document).ready(function(){
   // FUNCTION TO PERFORM POSTCODE LOOKUP
   function postcodeLookup(postcode) {
     // IF !NUMERIC
-    if(isNaN(postcode)){
-      $.getJSON( "http://api.geonames.org/findNearbyPostalCodesJSON?country=ca&radius=16&username=spotbrand&postalcode=" + "postcode", function(data) {
-        var postalCodes = [];
-        for (var i = 0; i < data.postalCodes.length; i++) {
-          postalCodes.push(data.postalCodes[i]['postalCode']);
-        }
-        console.log(postalCodes);
-        // FOR EACH ZIPCODE, QUERY STORE/VELOFIX CONDITIONS
-        $.each(postalCodes, function(key, value) {
-          var postcode = Number.parseInt(value);
-          // QUERY WES'S API
-          $.getJSON("http://departmentofscience.com/clients/spot/query.php?postcode=" + postcode, function(result){
-            console.log(result);
-          //   // IF STORE AND VELOFIX, SET COOKIE ACCORDINGLY
-          //   // ELSE IF STORE AND !VELOFIX
-          //   // ELSE IF !STORE AND VELOFIX, SET COOKIE ACCORDINGLY
-          //   // ELSE IF !STORE AND !VELOFIX, SET COOKIE ACCORDINGLY
-          })
-        })
-      });
-    }
+    // if(isNaN(postcode)){
+    //   $.getJSON( "http://api.geonames.org/findNearbyPostalCodesJSON?country=ca&radius=16&username=spotbrand&postalcode=" + "postcode", function(data) {
+    //     var postalCodes = [];
+    //     for (var i = 0; i < data.postalCodes.length; i++) {
+    //       postalCodes.push(data.postalCodes[i]['postalCode']);
+    //     }
+    //     console.log(postalCodes);
+    //     // FOR EACH ZIPCODE, QUERY STORE/VELOFIX CONDITIONS
+    //     $.each(postalCodes, function(key, value) {
+    //       var postcode = Number.parseInt(value);
+    //       // QUERY WES'S API
+    //       $.getJSON("http://departmentofscience.com/clients/spot/query.php?postcode=" + postcode, function(result){
+    //         console.log(result);
+    //       //   // IF STORE AND VELOFIX, SET COOKIE ACCORDINGLY
+    //       //   // ELSE IF STORE AND !VELOFIX
+    //       //   // ELSE IF !STORE AND VELOFIX, SET COOKIE ACCORDINGLY
+    //       //   // ELSE IF !STORE AND !VELOFIX, SET COOKIE ACCORDINGLY
+    //       })
+    //     })
+    //   });
+    // }
     $.getJSON( "http://api.geonames.org/findNearbyPostalCodesJSON?country=us&radius=16&username=spotbrand&postalcode=" + "postcode", function(data) {
       var postalCodes = [];
       for (var i = 0; i < data.postalCodes.length; i++) {
